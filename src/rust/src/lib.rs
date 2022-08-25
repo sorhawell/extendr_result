d036b2c23f3e91086a3ca4d7fb76a934226b5aeb
sorhawell <sorhawell@gmail.com>
2022-08-25 09:53:20 +0200

    comments

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,8 +1,8 @@
 use extendr_api::prelude::{extendr, extendr_module, list, GetSexp, IntoRobj}; // or just use extendr_api::prelude::*;
 use extendr_api::NULL;
 
-//convert rust Result into either list(ok=ok_value,err=NULL) or list(ok=NULL,err=err_string)
-//use custom unwrap-function on R side or any custom code to read results and/or throw errors.
+//convert any extendr exportalbe Result into either list(ok=ok_value,err=NULL) or list(ok=NULL,err=err_string)
+//... where error implements Display. This error traitbound could be resolved on R side instead.
 pub fn r_result_list<T, E>(x: std::result::Result<T, E>) -> list::List
 where
     T: std::fmt::Debug + IntoRobj,
@@ -15,19 +15,20 @@ where
     }
 }
 
+//some function using the r_result_list generic function to export
 #[extendr]
 fn rust_hello_result(is_ok: bool) -> list::List {
+    //some silly logic to emulate Result
     let result = if is_ok {
         Ok(42i32)
     } else {
         Err("rust world error: is_ok must be set to true")
     };
+
+    //instead of returning Result<T,E> then wrap like this and return list::List
     r_result_list(result)
 }
 
-// Macro to generate exports.
-// This ensures exported functions are registered with R.
-// See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod helloresult;
     fn rust_hello_result;
